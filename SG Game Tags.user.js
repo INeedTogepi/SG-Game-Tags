940626d233b1ee0c08eafc24f45eaf46060a78c3
Christhofer Natalius <christhofernatalius@gmail.com>
2016-03-17 16:07:43 +0700

    bundle & trading card status selesai

--- SG Game Tags.user.js
+++ SG Game Tags.user.js
@@ -28,6 +28,7 @@ myCSS = '<style> \
 			font-size: 8pt; \
 			margin: 3px; \
 			text-shadow: none; \
+			display: none; \
 		} \
 		.tcards { background-color: #3AA435; } \
 		.bundle { background-color: #f44336; } \
@@ -39,54 +40,67 @@ $("head").append(myCSS);
 /* Constant Variables */
 const linkCard = "http://www.steamcardexchange.net/index.php?inventorygame-appid-";
 const linkBundle = "http://www.steamgifts.com/bundle-games/search?q=";
+
+ //use cors.io to bypass CORS problem, but it can't receive 2 or more GET params
+//should use http://store.steampowered.com/api/appdetails?filters=categories&appids= for less data request, but somehow has CORS problem
 const linkGameAPI = "http://cors.io/?u=http://store.steampowered.com/api/appdetails?appids=";
+const linkPackAPI = "http://cors.io/?u=http://store.steampowered.com/api/packagedetails?packageids=";
 
-const SGhome = "http://www.steamgifts.com/";
+const ClassCard = "tcards";
+const TitleCard = "This game has trading cards";
+const TextCard = "Trading Card";
 
-const regexuser = /http:\/\/www.steamgifts.com\/user\//;
-
-const tCardTitle = "This game has trading cards";
-const tBundleTitle = "This game is considered as bundled by Steamgifts";
-
-
-/* Create Trading Card Tag */
-var tCards = document.createElement('a');
-tCards.setAttribute("id", "tags");
-tCards.setAttribute("class", "tcards");
-tCards.setAttribute("target", "_blank");
-tCards.setAttribute("title", tCardTitle);
-tCards.innerHTML = "Trading Cards";
-
-/* Create Bundle Tag */
-var tBundle = document.createElement('a');
-tBundle.setAttribute("href", linkBundle);
-tBundle.setAttribute("id", "tags");
-tBundle.setAttribute("class", "bundle");
-tBundle.setAttribute("target", "_blank");
-tBundle.setAttribute("title", tBundleTitle);
-tBundle.innerHTML = "Bundled";
-
-var currLoc = window.location.href;
+const ClassBundle = "bundle";
+const TitleBundle = "This game is considered as bundled by Steamgifts";
+const TextBundle = "Bundled";
 
 main();
 
 function main()
 {
 	//shows trading card tag in featured game (header)
-	if(!regexuser.test(currLoc)) //exclude user page for getting featured appID
+	var currLoc = window.location.href.split("/");
+	if(currLoc[currLoc.length-2] != "user") //exclude user page for getting featured appID
 	{	
 		var ID = getAppIDfromImg($(".global__image-outer-wrap img")[0].src);
-		getTradingCardStatus($(".featured__heading"), ID);
-	}
+		var Name = $(".featured__heading__medium").text();
+		var target = $(".featured__heading");
 
+		var tagCard = createTag(ClassCard, TitleCard, TextCard, linkCard+ID, target);
+		var tagBundle = createTag(ClassBundle, TitleBundle, TextBundle, linkBundle+Name, target);
+
+		getTradingCardStatus(tagCard, ID);
+		getBundleStatus(tagBundle, ID, Name);
+	}
 
 	//looping for each games shown
 	$(".giveaway__row-inner-wrap").each(function(index, element)
 	{
 		var ID = getAppIDfromLink($("a.giveaway__icon")[index].href);
-		
-		getTradingCardStatus($(".giveaway__heading:nth-child(" + index + ")"), ID);
+		var Name = $($(".giveaway__heading__name")[index]).text();
+		var target = $(element).find(".giveaway__heading");
+
+		var tagCard = createTag(ClassCard, TitleCard, TextCard, linkCard+ID, target);
+		var tagBundle = createTag(ClassBundle, TitleBundle, TextBundle, linkBundle+Name, target);
+
+		getTradingCardStatus(tagCard, ID);
+		getBundleStatus(tagBundle, ID, Name);
 	});
+}
+
+function createTag(_class, title, text, href, divTarget)
+{
+	var tag = document.createElement("a");
+	tag.setAttribute("id", "tags");
+	tag.setAttribute("target", "_blank");
+	tag.setAttribute("class", _class);
+	tag.setAttribute("title", title);
+	tag.setAttribute("href", href);
+	tag.innerHTML = text;
+
+	divTarget.append(tag);
+
+	return tag;
 }
 
 function getAppIDfromImg(link)
@@ -105,6 +119,7 @@ function getAppIDfromLink(link)
 
 function getTradingCardStatus(elems, appID)
 {
+	//TODO: Check if the game is saved, if no then request to steam
     $.ajax({
         url: linkGameAPI+appID,
         datatype: "jsonp",
@@ -115,14 +130,33 @@ function getTradingCardStatus(elems, appID)
             {
                 if(obj[i].id == "29")
                 {
-                	tCards.setAttribute("href", linkCard+appID);
-                	//console.log($(".giveaway__heading")[index]);
-                	//console.log(elems);
-                	console.log(appID);
-				elems.append(tCards);
-				break;
+                	//TODO : Save appID + true ke local cache
+                	$(elems).css("display", "block");
+					break;
                 }
             }
+            //TODO : Save appID + false + expire time ke local cache
         }
     });
+}
+
+function getBundleStatus(elems, appID, appName)
+{
+	//TODO: Check if the game is saved, if no then request to steam
+	$.get( linkBundle+appName, function( data ) {
+		var gamesfound = $(data).find(".table__column__secondary-link");
+		for(i=0; i<$(gamesfound).length; i++)
+		{
+			var url = $(gamesfound)[i].href;
+			var ID = getAppIDfromLink(url);
+
+			if(appID == ID)
+			{
+				//TODO : Save appID + true ke local cache
+				$(elems).css("display", "block");
+				break;
+			}
+		}
+		//TODO : Save appID + false + expire time ke local cache
+	});
 }
